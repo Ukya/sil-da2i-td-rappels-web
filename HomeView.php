0656e7286d9821330fff92342668538ff114e2d2
Ukya <44288759+Ukya@users.noreply.github.com>
2018-12-05 13:16:51 +0100

    Add Home

---
+++ HomeView.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style_film.css" />
+        <title>Info Film</title>
+    </head>
+
+    <?php
+        function getBlock($file, $data = [])
+        {
+            require $file;
+        }       
+    ?>
+
+    <body>
+
+        <div id="bloc_page">
+
+            <?php getBlock('blocks/header.php') ?>
+
+            <div id="bloc_body">
+
+                <div id="bloc_sections">
+
+                    <div class="liste_index">
+                        <h1>Liste alphabétique des films</h1>
+                        <?php
+                            foreach ($films as $film) 
+                            {
+                                ?>
+                                <p>
+                                    <span><a href="movie.php?title=<?php echo $film['title']; ?>&amp;id=<?php echo $film['id']; ?>"><?php echo $film['title']; ?></a></span>
+                                </p>
+                                <?php
+                            } 
+                        ?>
+                    </div>
+
+                    <div class="liste_index">
+                        <h1>Liste alphabétique des réalisateurs</h1>
+                        <?php
+                            foreach ($directors as $director) 
+                            {
+                                ?>
+                                <p>
+                                    <span><a href="director.php?name=<?php echo $director['firstname'] . '_' . $director['lastname']; ?>&amp;id=<?php echo $director['id']; ?>"><?php echo $director['firstname'] . ' ' . $director['lastname']; ?></a></span>
+                                </p>
+                                <?php 
+                            } 
+                        ?>
+                    </div>
+                    
+                    <div class="liste_index">
+                        <h1>Liste alphabétique des acteurs</h1>
+                        <?php
+                            foreach ($actors as $actor) 
+                            {
+                                ?>
+                                <p>
+                                    <span><a href="director.php?name=<?php echo $actor['firstname'] . '_' . $actor['lastname']; ?>&amp;id=<?php echo $actor['id']; ?>"><?php echo $actor['firstname'] . ' ' . $actor['lastname']; ?></a></span>
+                                </p>
+                                <?php 
+                            } 
+                        ?>
+                    </div>
+                    
+                </div>
+                
+            </div>
+
+            <?php getBlock('blocks/footer.php') ?>
+
+        </div>
+
+    </body>
+
+</html>
+
